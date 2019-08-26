f4c0140141d589e0832fb7eb1fa214bce7762018
Brett O'Connor <37229165+caretakerx@users.noreply.github.com>
2019-08-26 09:22:37 -0700

    Brett O'Connor Designs

---
+++ build/precache-manifest.ab9a7dbea9a64708fdb54d48b944f2bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa3c938c37d8b402e32e4fbaad0eb4d",
+    "url": "/react-app/build/index.html"
+  },
+  {
+    "revision": "4896f08518dc806e1798",
+    "url": "/react-app/build/static/css/main.f0f18f1e.chunk.css"
+  },
+  {
+    "revision": "76be2ce44b7dab5e8af5",
+    "url": "/react-app/build/static/js/2.d9b5aa11.chunk.js"
+  },
+  {
+    "revision": "4896f08518dc806e1798",
+    "url": "/react-app/build/static/js/main.d109f5d7.chunk.js"
+  },
+  {
+    "revision": "d23d3d2ef60d3a795aec",
+    "url": "/react-app/build/static/js/runtime~main.3e303395.js"
+  }
+]);
